537355c2a6f7e0c91ec90f3c036ac7e7fc857b57
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2023-10-13 06:45:36 +0500

    routing complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Article from "./Pages/Article";
 import ArticlesList from "./Pages/ArticlesList";
+import Navbar from "./Components/Navbar";
+import NotFound from "./Pages/NotFound";
+
 
 const App = () => {
   return (
-    <div className="max-w-screen-md mx-auto pt-20">
-      <Router>
+    <Router>
+      <Navbar />
+      <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/articles-list" element={<ArticlesList />} />
-          <Route path="/article" element={<Article />} />
+          <Route path="/article/:name" element={<Article />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 };
 
--- src/Pages/ArticlesList.jsx
+++ src/Pages/ArticlesList.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import articleContent from "./article-content";
 
+import Articles from "../Components/Articles";
 const ArticlesList = () => {
   return (
-    <div>
+    <>
       <h1 className="mb-20 sm:text-4xl text-2xl my-6 text-grey-900 font-bold">
         Articles
       </h1>
-    </div>
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-wrap m-4">
+          <Articles articles={articleContent}/>
+        </div>
+      </div>
+    </>
   );
 };
 
